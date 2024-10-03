abee4e9722413c3c6c98a35202d106cfdd1c075e
WEBSERVER\henry <henryma14@gmail.com>
2024-10-02 19:47:29 -0700

    fix: fixed merge conflict

--- frontend/react_sql_app/src/App.js
+++ frontend/react_sql_app/src/App.js
@@ -78,11 +78,7 @@ const App = () => {
   const [pageSize, setPageSize] = useState(10)
   const [drawNumber, setDrawNumber] = useState(1)
 
-<<<<<<< HEAD
  
-=======
-  /*
->>>>>>> 31a84f5a75806d13ab4e82759877fab54ed84325
 const url10 = 'http://ep.lottotry.com:5001/api/lotto/getCurrentDrawNumber?lotto_name=' + lottoName
 const url = 'http://ep.lottotry.com:5001/api/openai'
 const url4 = 'http://ep.lottotry.com:5001/api/lotto/allNumbers?lotto_name=' + lottoName + '&page_number=' + page + '&page_size=' + pageSize + '&drawNumber=' + drawNumber
@@ -90,7 +86,6 @@ const url5 = 'http://ep.lottotry.com:5001/api/lotto/predict?lotto_name=' + lotto
 const url9 = 'http://ep.lottotry.com:5001/api/lotto/potential_draws?lotto_name=' + lottoName + '&columns=' + potentialColumns + '&page_size=' + pageSize + '&drawNumber=' + drawNumber
 const url7 = 'http://ep.lottotry.com:5001/api/lotto/lottoDraws?lotto_name=' + lottoName + '&page_number=' + page + '&page_size=' + pageSize + '&drawNumber=' + drawNumber
 const url8 = 'http://ep.lottotry.com:5001/api/lotto/numberDraws?lotto_name=' + lottoName + '&page_number=' + page + '&page_size=' + pageSize + '&drawNumber=' + drawNumber
-<<<<<<< HEAD
 
  /*
 
@@ -104,21 +99,6 @@ const url8 = 'http://ep.lottotry.com:5001/api/lotto/numberDraws?lotto_name=' + l
 
 */
 
-=======
-
-*/
-
-  const url10 = 'http://127.0.0.1:5001/api/lotto/getCurrentDrawNumber?lotto_name=' + lottoName
-  const url = 'http://127.0.0.1:5001/api/openai'
-  const url4 = 'http://127.0.0.1:5001/api/lotto/allNumbers?lotto_name=' + lottoName + '&page_number=' + page + '&page_size=' + pageSize + '&drawNumber=' + drawNumber
-  const url5 = 'http://127.0.0.1:5001/api/lotto/predict?lotto_name=' + lottoName + '&columns=' + lottoColumns + '&drawNumber=' + drawNumber
-  const url9 = 'http://127.0.0.1:5001/api/lotto/potential_draws?lotto_name=' + lottoName + '&columns=' + potentialColumns + '&page_size=' + pageSize + '&drawNumber=' + drawNumber
-  const url7 = 'http://127.0.0.1:5001/api/lotto/lottoDraws?lotto_name=' + lottoName + '&page_number=' + page + '&columns=' + lottoColumns + '&page_size=' + pageSize + '&drawNumber=' + drawNumber
-  const url8 = 'http://127.0.0.1:5001/api/lotto/numberDraws?lotto_name=' + lottoName + '&page_number=' + page + '&page_size=' + pageSize + '&drawNumber=' + drawNumber
-
-
-
->>>>>>> 31a84f5a75806d13ab4e82759877fab54ed84325
 
   useEffect(() => {
     const getCurrentDrawNumber = async () => {
