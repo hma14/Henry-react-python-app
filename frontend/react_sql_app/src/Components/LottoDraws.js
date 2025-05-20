dd7a06bd5b7167f023dede9a9c8b6741d25adea2
hma14 <henryma14@gmail.com>
2025-05-20 15:02:50 -0700

    td width changes

--- frontend/react_sql_app/src/Components/LottoDraws.js
+++ frontend/react_sql_app/src/Components/LottoDraws.js
@@ -1,71 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Table } from 'react-bootstrap'
-import '../App.css'
-
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Table } from "react-bootstrap";
+import "../App.css";
 
 function LottoDraws(props) {
-  const { endpoint, page_number, columns } = props
+  const { endpoint, page_number, columns } = props;
   const [lottoData, setData] = useState([]);
 
   useEffect(() => {
     // Fetch data from the specified endpoint
-    axios.get(endpoint)
-      .then(response => {
+    axios
+      .get(endpoint)
+      .then((response) => {
         setData(response.data.data);
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   }, [page_number, columns, endpoint]);
 
   const getHeader = () => {
     return (
-      
-      <thead className="table-danger text-center">      
+      <thead className="table-danger text-center">
         <tr>
           <th className="text-light bg-info">Draws</th>
           <th className="text-light bg-info">Date</th>
-          {lottoData.slice(0, 1).map(row => row.Numbers.sort((a, b) => (a - b)).map((no) => (no.Value < columns ?
-            (<th key={no.Value} className='text-warning bg-success fst-italic'>{no.Value}</th>) :
-            (no.Value === columns ? (<th key={no.Value} className='text-warning bg-success fst-italic'>Bonus ({no.Value})</th>) : ''))))}
+          {lottoData.slice(0, 1).map((row) =>
+            row.Numbers.sort((a, b) => a - b).map((no) =>
+              no.Value < columns ? (
+                <th
+                  key={no.Value}
+                  className="text-warning bg-success fst-italic"
+                >
+                  {no.Value}
+                </th>
+              ) : no.Value === columns ? (
+                <th
+                  key={no.Value}
+                  className="text-warning bg-success fst-italic"
+                >
+                  Bonus
+                </th>
+              ) : (
+                ""
+              )
+            )
+          )}
         </tr>
-      </thead >
-    )
-  }
-
+      </thead>
+    );
+  };
 
   return (
-
     <div>
-      {lottoData &&
-        <Table striped bordered hover  className="table-primary mb-4" >
+      {lottoData && (
+        <Table striped bordered hover className="table-primary mb-4">
           {console.log(columns)}
           {getHeader()}
-          <tbody className='fw-bold' >
-            {lottoData.map(row =>
+          <tbody className="fw-bold">
+            {lottoData.map((row) => (
               <tr key={row.DrawNumber}>
                 <td className="text-light bg-primary">{row.DrawNumber}</td>
                 <td className="text-light bg-success">{row.DrawDate}</td>
-                {row.Numbers
-                  .sort((a, b) => (b.IsBonusNumber === true && a.IsBonusNumber === false && (b.Value - a.Value)))
-                  .sort((a, b) => (a.IsBonusNumber === false && b.IsBonusNumber === false && (a.Value - b.Value)))
-                  .map(no => no.IsHit === true ?
-                    (<td className='text-primary bg-greenyellow' key={no.Value}>
-                      {no.IsBonusNumber === true ? <span className='fs-5 text-success'>{no.Value}</span> : <span className='fs-5 text-primary'>{no.Value}</span>}
-                      (<span className='text-danger fst-italic'>{no.NumberOfDrawsWhenHit}</span>)
-                      (<span className='text-secondary fst-italic'>{no.TotalHits}</span>)</td>)
-                    : '')}
+                {row.Numbers.sort(
+                  (a, b) =>
+                    b.IsBonusNumber === true &&
+                    a.IsBonusNumber === false &&
+                    b.Value - a.Value
+                )
+                  .sort(
+                    (a, b) =>
+                      a.IsBonusNumber === false &&
+                      b.IsBonusNumber === false &&
+                      a.Value - b.Value
+                  )
+                  .map((no) =>
+                    no.IsHit === true ? (
+                      <td
+                        className="text-primary bg-greenyellow"
+                        key={no.Value}
+                      >
+                        {no.IsBonusNumber === true ? (
+                          <span className="fs-5 text-success">{no.Value}</span>
+                        ) : (
+                          <span className="fs-5 text-primary">{no.Value}</span>
+                        )}
+                        (
+                        <span className="text-danger fst-italic">
+                          {no.NumberOfDrawsWhenHit}
+                        </span>
+                        ) (
+                        <span className="text-secondary fst-italic">
+                          {no.TotalHits}
+                        </span>
+                        )
+                      </td>
+                    ) : (
+                      ""
+                    )
+                  )}
               </tr>
-            )}
+            ))}
           </tbody>
           {getHeader()}
-        </Table>}
-
+        </Table>
+      )}
     </div>
-  )
+  );
 }
 
-export default LottoDraws
+export default LottoDraws;
--- frontend/react_sql_app/src/Components/NumberDrawsInDistance.js
+++ frontend/react_sql_app/src/Components/NumberDrawsInDistance.js
@@ -1,90 +1,104 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Table } from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Table } from "react-bootstrap";
 //import moment   from 'moment'
-import '../App.css'
-
-
+import "../App.css";
 
 function NumberDrawsInDistance(props) {
-  const { endpoint, rows } = props
-  const cols = 11
-  const newList = Array(rows).fill(0).map(r => new Array(cols + 1).fill(0))
-  const columns = Array.from(Array(cols + 1).keys()).slice(1, cols + 1)
+  const { endpoint, rows } = props;
+  const cols = 11;
+  const newList = Array(rows)
+    .fill(0)
+    .map((r) => new Array(cols + 1).fill(0));
+  const columns = Array.from(Array(cols + 1).keys()).slice(1, cols + 1);
 
- 
-  const [lottoData, setData] = useState([])
+  const [lottoData, setData] = useState([]);
 
   useEffect(() => {
     // Fetch data from the specified endpoint
-    axios.get(endpoint)
-      .then(response => {
+    axios
+      .get(endpoint)
+      .then((response) => {
         setData(response.data.data);
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   }, [rows, endpoint]);
 
-  
-
-  lottoData && lottoData.forEach((row, i) => {
-    let arr = new Array(cols).fill(0)
-    row.Numbers.forEach(no => {
-      if (no.IsHit === true && no.NumberOfDrawsWhenHit < cols) {
-        arr[no.NumberOfDrawsWhenHit]++
-      }
-    })
-    let sum = arr.reduce((a, b) => a = a + b, 0)
-    let obj = [row.DrawNumber, row.DrawDate, ...arr.slice(1, cols), sum]
-    newList[i] = obj
-  })
+  lottoData &&
+    lottoData.forEach((row, i) => {
+      let arr = new Array(cols).fill(0);
+      row.Numbers.forEach((no) => {
+        if (no.IsHit === true && no.NumberOfDrawsWhenHit < cols) {
+          arr[no.NumberOfDrawsWhenHit]++;
+        }
+      });
+      let sum = arr.reduce((a, b) => a + b, 0);
+      let obj = [row.DrawNumber, row.DrawDate, ...arr.slice(1, cols), sum];
+      newList[i] = obj;
+    });
 
   const getHeader = () => {
     return (
-
       <thead className="table-danger text-center">
         <tr>
           <th className="text-light bg-info">Draws</th>
           <th className="text-light bg-info">Date</th>
-          {columns && columns.map((no) => no < cols ?
-            (<th key={no} className='text-warning bg-success fst-italic'>{no}</th>) :
-            (no === cols ?
-              (<th className='text-light bg-info'>Sum of Hits</th>) :
-              ''))}
+          {columns &&
+            columns.map((no) =>
+              no < cols ? (
+                <th key={no} className="text-warning bg-success fst-italic">
+                  {no}
+                </th>
+              ) : no === cols ? (
+                <th className="text-light bg-info">Sum of Hits</th>
+              ) : (
+                ""
+              )
+            )}
         </tr>
       </thead>
-    )
-  }
-
-
-
+    );
+  };
 
   return (
     <div>
-
-      {lottoData && lottoData.length > 0 &&
-        <Table responsive className="table-primary mb-4" size="sm" hover="true" >
+      {lottoData && lottoData.length > 0 && (
+        <Table responsive className="table-primary mb-4" size="sm" hover="true">
           {getHeader()}
-          <tbody className='fw-bold' >
-            {newList && newList.length > 0 && newList.map((row) =>
-              <tr key={row[0]}>
-                <td className="text-warning bg-primary">{row[0]}</td>
-                <td className="text-warning bg-success">{row[1]}</td>
-                {row.slice(2, cols + 1).map(no => no > 0 ?
-                  <td className='text-danger bg-warning' key={no}>{no}</td> :
-                  <td className='bg-greenyellow'></td>
-                )}
-                {row[cols + 1] < 4 ?
-                  <td className="text-light bg-success">{row[cols + 1]}</td> :
-                  <td className="text-warning bg-success">{row[cols + 1]}</td>
-                }
-              </tr>
-            )}
+          <tbody className="fw-bold">
+            {newList &&
+              newList.length > 0 &&
+              newList.map((row) => (
+                <tr key={row[0]}>
+                  <td className="text-warning bg-primary">{row[0]}</td>
+                  <td className="text-warning bg-success">{row[1]}</td>
+                  {row.slice(2, cols + 1).map((no) =>
+                    no > 0 ? (
+                      <td className="text-danger bg-warning wider-td" key={no}>
+                        {no}
+                      </td>
+                    ) : (
+                      <td className="bg-greenyellow wider-td"></td>
+                    )
+                  )}
+                  {row[cols + 1] < 4 ? (
+                    <td className="text-light bg-success text-center">
+                      {row[cols + 1]}
+                    </td>
+                  ) : (
+                    <td className="text-warning bg-success text-center">
+                      {row[cols + 1]}
+                    </td>
+                  )}
+                </tr>
+              ))}
           </tbody>
           {getHeader()}
-        </Table>}
+        </Table>
+      )}
     </div>
-  )
+  );
 }
-export default NumberDrawsInDistance
+export default NumberDrawsInDistance;
