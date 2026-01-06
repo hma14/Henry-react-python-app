0de6edf3163041afde29c684c1a0674068c59f0b
hma14 <henryma14@gmail.com>
2026-01-06 14:26:02 -0800

    bug fixing

--- frontend/react_sql_app/src/Components/ApiNumbers.js
+++ frontend/react_sql_app/src/Components/ApiNumbers.js
@@ -278,11 +278,12 @@ const ApiNumbers = (props) => {
           </Table>
         )}{" "}
       </div>
-      {data.length === 0 && (
-        <div className="text-center text-red-600 text-3xl font-semibold mt-8">
-          No data available
-        </div>
-      )}
+      {data == null ||
+        (data.length === 0 && (
+          <div className="text-center text-red-600 text-3xl font-semibold mt-8">
+            No data available
+          </div>
+        ))}
     </>
   );
 };
